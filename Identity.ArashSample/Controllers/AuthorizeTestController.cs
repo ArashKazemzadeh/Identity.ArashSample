011ad91bb58ef59f6467696c708ec546f66e7c49
Arash Kazemzadeh <kazemzade.arash13@gmail.com>
2023-05-15 12:59:00 +0330

    Add project files.

---
+++ Identity.ArashSample/Controllers/AuthorizeTestController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Identity.Bugeto.Controllers
+{
+    [Authorize(Roles = "Admin,Operator")]
+    //[Authorize(Roles = "")]
+    public class AuthorizeTestController : Controller
+    {
+     
+        public string Index()
+        {
+            return "Index";
+        }  
+        
+        [Authorize(Roles = "Operator")]
+        public string Edit()
+        {
+            return "Index";
+        }
+    }
+}
---
+++ Identity.ArashSample/Helpers/BlogRequirement.cs
@@ -0,0 +1,25 @@
+﻿using Identity.Bugeto.Models.Dto.Blog;
+using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Identity.Bugeto.Helpers
+{
+    public class BlogRequirement: IAuthorizationRequirement
+    {
+    }
+
+    public class IsBlogForUserAuthorizationHandler : AuthorizationHandler<BlogRequirement, BlogDto>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, BlogRequirement requirement, BlogDto resource)
+        {
+           if(context.User.Identity?.Name == resource.UserName)
+            {
+                context.Succeed(requirement);
+            }
+            return Task.CompletedTask;
+        }
+    }
+}
